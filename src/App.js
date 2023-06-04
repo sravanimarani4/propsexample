50428479b4841c15081727482f822961c678ffb8
sravanimaragani4 <sravanimaragani4@gmail.com>
2023-06-04 16:58:58 +0530

    props example of card.

--- src/App.js
+++ src/App.js
@@ -1,152 +1,71 @@
 import React from 'react'
 import "./File.css";
+import data from './Data';
+
+console.log(data);
 
 
-const data = [
-  {
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-  },
-  {
-    "albumId": 1,
-    "id": 2,
-    "title": "reprehenderit est deserunt velit ipsam",
-    "url": "https://via.placeholder.com/600/771796",
-    "thumbnailUrl": "https://via.placeholder.com/150/771796"
-  },
-  {
-    "albumId": 1,
-    "id": 3,
-    "title": "officia porro iure quia iusto qui ipsa ut modi",
-    "url": "https://via.placeholder.com/600/24f355",
-    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
-  },
-  {
-    "albumId": 1,
-    "id": 4,
-    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-    "url": "https://via.placeholder.com/600/d32776",
-    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
-  },
-  {
-    "albumId": 1,
-    "id": 5,
-    "title": "natus nisi omnis corporis facere molestiae rerum in",
-    "url": "https://via.placeholder.com/600/f66b97",
-    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
-  },
-  {
-    "albumId": 1,
-    "id": 6,
-    "title": "accusamus ea aliquid et amet sequi nemo",
-    "url": "https://via.placeholder.com/600/56a8c2",
-    "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
-  },
-  {
-    "albumId": 1,
-    "id": 7,
-    "title": "officia delectus consequatur vero aut veniam explicabo molestias",
-    "url": "https://via.placeholder.com/600/b0f7cc",
-    "thumbnailUrl": "https://via.placeholder.com/150/b0f7cc"
-  },
-  {
-    "albumId": 1,
-    "id": 8,
-    "title": "aut porro officiis laborum odit ea laudantium corporis",
-    "url": "https://via.placeholder.com/600/54176f",
-    "thumbnailUrl": "https://via.placeholder.com/150/54176f"
-  },
-  {
-    "albumId": 1,
-    "id": 9,
-    "title": "qui eius qui autem sed",
-    "url": "https://via.placeholder.com/600/51aa97",
-    "thumbnailUrl": "https://via.placeholder.com/150/51aa97"
-  },
-  {
-    "albumId": 1,
-    "id": 10,
-    "title": "beatae et provident et ut vel",
-    "url": "https://via.placeholder.com/600/810b14",
-    "thumbnailUrl": "https://via.placeholder.com/150/810b14"
-  },
-  {
-    "albumId": 1,
-    "id": 11,
-    "title": "nihil at amet non hic quia qui",
-    "url": "https://via.placeholder.com/600/1ee8a4",
-    "thumbnailUrl": "https://via.placeholder.com/150/1ee8a4"
-  },
-  {
-    "albumId": 1,
-    "id": 12,
-    "title": "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
-    "url": "https://via.placeholder.com/600/66b7d2",
-    "thumbnailUrl": "https://via.placeholder.com/150/66b7d2"
-  },
-  {
-    "albumId": 1,
-    "id": 13,
-    "title": "repudiandae iusto deleniti rerum",
-    "url": "https://via.placeholder.com/600/197d29",
-    "thumbnailUrl": "https://via.placeholder.com/150/197d29"
-  },
-  {
-    "albumId": 1,
-    "id": 14,
-    "title": "est necessitatibus architecto ut laborum",
-    "url": "https://via.placeholder.com/600/61a65",
-    "thumbnailUrl": "https://via.placeholder.com/150/61a65"
-  },
-  {
-    "albumId": 1,
-    "id": 15,
-    "title": "harum dicta similique quis dolore earum ex qui",
-    "url": "https://via.placeholder.com/600/f9cee5",
-    "thumbnailUrl": "https://via.placeholder.com/150/f9cee5"
-  },
-]
+
 
 function App() {
   return (
-    <div className='posts-container'>
-     {/* <Profile/>
-     <Profile/>
-     <Profile/> */}
-     {
-      data.map((eachObjt)=>{
-        const {id,title,url,thumbnailUrl} = eachObjt;
-        return(
-            <Profile
-            key={id}
-            id={id}
-            thumbnailUrl={thumbnailUrl}
-            title={title}
-            url={url}
-            />
-        )
-      })
-     }
-    </div>
+    <main  className='container'>
+      <ul className='comments-container'>
+        {/* <li className='comment'>
+          <div className='comments-header'>
+            <h3 className='email'>email.email@.com</h3>
+            <h3 className='name'>example1</h3>
+          </div>
+          <p className='message'>
+          n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to 
+          demonstrate the visual form of a document or a typeface without relying on meaningful content
+          </p>
+        </li>
+
+        <li className='comment'>
+          <div className='comments-header'>
+            <h3 className='email'>email.email@.com</h3>
+            <h3>example2</h3>
+          </div>
+          <p className='message'>
+          n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to  
+          demonstrate the visual form of a document or a typeface without relying on meaningful content
+          </p>
+        </li>
+
+        <li className='comment'>
+          <div className='comments-header'>
+            <h3 className='email'>email.email@.com</h3>
+            <h3>example3</h3>
+          </div>
+          <p className='message'>
+          n publishing and graphic design, Lorem ipsum is a placeholder text 
+          commonly used to demonstrate the visual form of a document or 
+          a typeface without relying on meaningful content.
+          </p>
+        </li> */}
+        {
+          data.map((eachObj) => {
+            const {id,email,name,body} = eachObj;
+            return(
+              <li key={id} className='comment'>
+                <div className='comments-header'>
+                  <h3 className='email'>{email}</h3>
+                  <h3 className='name'>{name}</h3>
+               </div>
+               <p className='message'>{body}</p>
+              </li>
+            )
+          })
+        }
+     </ul>
+    </main>
+    
   )
 }
 
 
 
-function Profile(props) {
-  const {id,title,thumbnailUrl,url} = props;
-  return (
-    <article className='profile-card'>
-     {/* <img src='https://via.placeholder.com/150/24f355' alt=''/> */}
-     <img src={thumbnailUrl} alt=''/>
-     <h2 className='title'>{title}</h2>
-     {/* <a href='https://via.placeholder.com/600/24f355' className='button'>details</a> */}
-     <a href={url} className='button'>details</a>
-    </article>
-  )
-}
+
 
 export default App
